843fb2b4aa8d1ac54c04ca66b0b84a71cf5d3ddb
Erik Kostashuk <erikkostashuk@gmail.com>
2024-09-16 21:30:32 -0400

    .

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <h3>Located in Toronto, Canada 🇨🇦</h3>
       </div>
       <div className="links">
-        <a href="https://github.com/erikkostashuk" target="_blank" rel="noopener noreferrer">GitHub </a>
+        <a href="https://github.com/erikkostashuk" target="_blank" rel="noopener noreferrer">GitHub</a>
         <a href="https://linkedin.com/in/erikkostashuk" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         <a href="https://.com" target="_blank" rel="noopener noreferrer">Dev Blog</a>
       </div>
