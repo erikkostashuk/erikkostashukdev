13f0f5b249fa08944bf6332374c9c50591664855
Erik Kostashuk <erikkostashuk@gmail.com>
2024-09-16 21:12:25 -0400

    LinkTree stylist react site setup on GH pages

---
+++ erikkostashukdev/src/App.js
@@ -0,0 +1,22 @@
+// src/App.js
+import React from 'react';
+import './App.css'; // We'll create this file for custom styles
+
+function App() {
+  return (
+    <div className="app">
+      <div className="header">
+        <h1>Erik Kostashuk</h1>
+        <h3>Software Developer</h3>
+        <h3>Located in Toronto, Canada 🇨🇦</h3>
+      </div>
+      <div className="links">
+        <a href="https://github.com/erikkostashuk" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://linkedin.com/in/erikkostashuk" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://devwitherik.com" target="_blank" rel="noopener noreferrer">Dev Blog</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
