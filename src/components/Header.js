ffb05bc2b7c46e8dd34ae49300ec60920e39a070
Erik Kostashuk <erikkostashuk@gmail.com>
2025-10-31 17:09:37 -0400

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,9 @@ const Header = ({ isDay }) => {
   return (
     <div className="header">
       <h1>Erik Kostashuk</h1>
-      <h3>Software Developer</h3>
+      <h3>Software Developer at <a href="https://www.ssense.com" target="_blank" rel="noopener noreferrer" className="inline-link">SSENSE</a></h3>
       <h3>Located in Toronto, Canada 🇨🇦</h3>
+      <h3>Building <a href="https://www.geoclip.fun/" target="_blank" rel="noopener noreferrer" className="inline-link">GeoClip.fun</a>, a video-powered geography guessing game</h3>
     </div>
   );
 };
